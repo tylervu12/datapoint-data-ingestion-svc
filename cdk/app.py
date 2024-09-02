e27269dcc36aa93b911a7a22176552e7728d1950
tyler-acero <tylervu12@gmail.com>
2024-09-01 20:51:45 -0700

    deploy embedding model into AWS
    the main commits to look for here are in the folders for src and tests. deploy_embedding_model_sagemaker.py deploys https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2 onto AWS sagemaker. test_deploy_embedding_model_sagemaker.py is the unit test for this deployment to make sure it runs smoothly

--- cdk/app.py
+++ cdk/app.py
@@ -1,28 +1,11 @@
 #!/usr/bin/env python3
 import os
-
 import aws_cdk as cdk
-
-from cdk.cdk_stack import CdkStack
-
+from cdk.lambda_layers_stack import LambdaLayersStack
 
 app = cdk.App()
-CdkStack(app, "CdkStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+LambdaLayersStack(app, "LambdaLayersStack",
+    env=cdk.Environment(account='891376972161', region='us-east-1'),
+)
 
 app.synth()
